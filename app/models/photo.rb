45d8943916c6568f8bac6b47145998fa72a8c671
BegoUrsus <layira2002@gmail.com>
2016-05-08 20:24:47 +0200

    Photos 2. Implement attributes

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -13,6 +13,14 @@ each `photo`.
 
 class Photo
   include Mongoid::Document
+  # attributes:
+  #   * a read/write attribute called `id` that will be of type `String` to hold the 
+  #   String form of the GridFS file `_id` attribute
+  #   * a read/write attribute called `location` that will be of type `Point` to hold 
+  #   the location information of where the photo was taken.
+  #   * a write-only (for now) attribute called `contents` that will be used to import and access
+  #   the raw data of the photo. This will have varying data types depending on 
+  #   context.
   attr_accessor :id, :location
   attr_writer :contents
 
