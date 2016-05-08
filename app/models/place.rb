ae74d28dcc5c302e60b8871806a5ff2cb777cfa2
BegoUrsus <layira2002@gmail.com>
2016-05-08 12:07:21 +0200

    2. Implement class method 'load_all'

--- app/models/place.rb
+++ app/models/place.rb
@@ -11,4 +11,18 @@ class Place
   	self.mongo_client['places']
   end
 
+  # class method called `load_all` that will bulk load a JSON document with 
+  # 'places' information into the places collection. This method must
+  # * accept a parameter of type `IO` with a JSON string of data
+  # * read the data from that input parameter (Note: this is similar handling an uploaded
+  # file within Rails)
+  # * parse the JSON string into an array of Ruby hash objects representing places 
+  # (Hint: `JSON.parse`)
+  # * insert the array of hash objects into the `places collection` (Hint: `insert_many`)
+  def self.load_all(file)
+  	docs = JSON.parse(file.read)
+  	collection.insert_many(docs)
+  end
+
+
 end  
